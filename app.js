3ac4a19852eaa593dfe59702b929928c3f0d73ab
Calvin Kreusser <CKreusser@gmail.com>
2017-10-24 15:04:58 -0600

    Trying too hrd

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log('listening at port: ', port);
-});
+app.use(express.static('public'));
+
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  res.send('hiiii!');
+  res.render('index');
 });
 
-app.use(function(req, res, next) {
-    console.log(req.url);
-    next();
+app.listen(port, () => {
+  console.log('listening at port: ', port);
 });
--- public/game.js
+++ public/game.js
@@ -2,19 +2,21 @@ var game = new Phaser.Game(750, 400, Phaser.CANVAS, 'gameContainer');
 
 var mainState = {
   preload: function() {
-    this.game.load.tilemap("tilemap", "assets/tile-sheets/test-level.json", null, Phaser.Tilemap.TILED_JSON);
-    this.game.load.image("tiles", "assets/tile_sheets/castle_tile_set_part1.png");
+    game.load.image("tiles", "/assets/tile-sheets/castle_tileset_part1.png");
+    game.load.tilemap("castle", "/assets/tile-sheets/hortest.json", Phaser.Tilemap.TILED_JSON);
   },
+
   create: function() {
     this.game.stage.backgroundColor = "#a9f0ff";
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    this.map = this.game.add.tilemap("tilemap");
-    this.map.addTilesetImage("tiles32", "tiles");
-    this.backgroundlayer = this.map.createLayer("backgroundLayer");
+    this.map = this.game.add.tilemap("castle");
+    this.map.addTilesetImage("castle", "tiles");
+    this.backgroundLayer = this.map.createLayer("backgroundLayer");
     this.groundLayer = this.map.createLayer("groundLayer");
     this.map.setCollisionBetween(1, 100, true, "groundLayer");
     this.groundLayer.resizeWorld();
   },
+
   update: function() {
     if (this.cursors.left.isDown)
     this.player.body.velocity.x = -200;
@@ -26,13 +28,10 @@ var mainState = {
     if (this.cursors.up.isDown && this.player.body.touching.down)
       this.player.body.velocity.y = -250;
 
-    game.physics.arcade.collide(this.player, this.walls);
-    game.physics.arcade.overlap(this.player, this.coins, this.takeCoin, null, this);
-    game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
+    this.game.physics.arcade.collide(this.player, this.walls);
+
   },
-  takeCoin: function(player, coin) {
-    coin.kill();
-  },
+
   restart: function() {
     game.state.start("main");
   },
